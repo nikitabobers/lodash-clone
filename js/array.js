6168bc7d728b93bbfb7f363b165ebb50665c17d8
nikita <nikita.bobers@gmail.com>
2020-04-29 12:49:14 +0200

    array difference, drop

--- js/array.js
+++ js/array.js
@@ -7,7 +7,7 @@
 // const chunk = (arr, size) => {
 //     let output = [];
 //     let newArr = [...arr];
-//     if (size <!-- <= 0) return newArr;
+//     if (size <= 0) return newArr;
 //     while (newArr.length) output.push(newArr.splice(0, size));
 //     return output;
 // };
@@ -38,18 +38,44 @@
 // .difference(array, [values])
 // Creates a new array concatenating array with any additional arrays and/or values.
 
+// const array = [1, 2, 3, 4];
+
+// const difference = (arr, values) => {
+//     const output = [];
+//     arr.filter(value => {
+//         if (values.includes(value)) {
+//             output.push(value);
+//         }
+//     });
+//     return output;
+// };
+
+// const result = difference(array, [2, 3]);
+// console.log(result);
+
+// ---------------------------------------------------------------------
+
+// .drop(array, [n])
+// Creates a slice of array with n elements dropped from the end.
+
 const array = [1, 2, 3, 4];
 
-const difference = (arr, values) => {
-    const output = [];
-    arr.filter(value => {
-        if (values.includes(value)) {
-            output.push(value);
-        }
-    });
+const drop = (arr, n) => {
+    const output = [...arr];
+    if (n <= 0 || n === undefined) {
+        return arr;
+    }
+    while (n > 0) {
+        n--;
+        output.pop();
+    }
     return output;
 };
 
-const result = difference(array, [2, 3]);
+const result = drop(array, 2);
 console.log(result);
+
 // ---------------------------------------------------------------------
+
+// .drop(array, [n])
+// Creates a slice of array with n elements dropped from the end.
