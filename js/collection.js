3345f0a74745a9ba12a951c261ffcf10ee47db0f
nikita <nikita.bobers@gmail.com>
2020-06-08 10:45:33 +0200

    collection groupBy/ keyBy

--- js/collection.js
+++ js/collection.js
@@ -48,7 +48,7 @@
 
 // ---------------------------------------------------------------------
 
-// filter(collection, predicate) 
+// filter(collection, predicate)
 // Iterates over elements of collection, returning an array of all elements predicate returns truthy for.
 
 // const users = [
@@ -72,7 +72,7 @@
 // ---------------------------------------------------------------------
 
 // .findLast()
-// const findLast = (array, func) => 
+// const findLast = (array, func) =>
 // 	array.reverse().find(func)
 
 // console.log(findLast(users, user => user.active))
@@ -81,24 +81,37 @@
 
 // .groupBy
 
-// const groupBy= (collection, func) => {
-  // const output = [];
+// const numbers = [6.1, 4.2, 6.3];
 
-  // for (let item of collection) {
-  //   output.push(Math.floor(item));
-  // }
+// const groupBy = numbers.reduce((obj, num) => {
+//   // Get the integer value of the number
+//   let int = Math.floor(num);
 
-  // let result = {};
-  // output.forEach((x) => (result[x] = (result[x] || 0) + 1));
-  // return output.forEach((x) => x * 2);
-  // return result;
-// };
+//   // If the integer doesn't already exist as a key in the object, create it
+//   if (!obj.hasOwnProperty(int)) {
+//     obj[int] = [];
+//   }
 
-// console.log(groupBy([6.1, 4.2, 6.3]));
+//   // Push the number to its integer key
+//   obj[int].push(num);
 
-const array = [1,2,3,4];
-const double = (number) => number * 2;
-const doubleArray = (array) => {
-	array.forEach(x => console.log(x + 2))
-}
-console.log(doubleArray(array))
+//   // Pass the object on to the next loop
+//   return obj;
+// }, {});
+
+// console.log(groupBy);
+
+// ---------------------------------------------------------------------
+
+// keyBy
+
+const array = [
+  { name: "mike", age: 22 },
+  { name: "bob", age: 25 },
+];
+
+const keyBy = (array, key) =>
+  array.reduce((r, x) => ({ ...r, [key ? x[key] : x]: x }), {});
+
+console.log(keyBy(array, "name"));
+
